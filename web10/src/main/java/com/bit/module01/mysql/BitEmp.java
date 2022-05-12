aedc21b85b44de6a2b81b75ba4c3e47655450b8f
mani703 <mani703@naver.com>
2022-05-12 17:33:56 +0900

    add

--- web10/src/main/java/com/bit/module01/mysql/BitEmp.java
+++ web10/src/main/java/com/bit/module01/mysql/BitEmp.java
@@ -11,7 +11,7 @@ import org.apache.log4j.Logger;
 public class BitEmp {
 	Logger log = Logger.getLogger(this.getClass());
 	
-	public ArrayList<?> getList() throws ClassNotFoundException, SQLException{
+	public ArrayList<EmpBean> getList() throws ClassNotFoundException, SQLException{
 		ArrayList<EmpBean> list = new ArrayList<>();
 		String sql = "select empno, ename, sal from emp order by empno desc";
 		
@@ -34,5 +34,16 @@ public class BitEmp {
 			}
 		}
 		return list;
+	}
+	
+	public boolean insertOne(int empno, String ename, int sal) throws ClassNotFoundException, SQLException {
+		String sql = "insert into emp (empno, ename, sal, hiredate) values (" + empno + ", '" + ename + "'," + sal + ", now())";
+		
+		try(
+				Connection conn = BitMysql.getConnection();
+				Statement stmt = conn.createStatement();
+				){
+			return stmt.executeUpdate(sql) > 0;
+		}
 	}
 }
