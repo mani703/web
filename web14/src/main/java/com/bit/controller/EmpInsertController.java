93bcf1ae823d4fc01786663b0685a85fa4b9fd7f
mani703 <mani703@naver.com>
2022-05-17 15:03:52 +0900

    insert

--- web14/src/main/java/com/bit/controller/EmpInsertController.java
+++ web14/src/main/java/com/bit/controller/EmpInsertController.java
@@ -17,34 +17,41 @@ public class EmpInsertController extends HttpServlet {
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String[] param = {
-				req.getParameter("empno"),
-				req.getParameter("ename"),
-				req.getParameter("sal")
+		String[] param= {
+				req.getParameter("empno")
+				,req.getParameter("ename")
+				,req.getParameter("sal")
 		};
-		int empno = Integer.parseInt(param[0].trim());
-		String ename = param[1].trim();
-		int sal = Integer.parseInt(param[2].trim());
+		int empno=Integer.parseInt(param[0].trim());
+		String ename=param[1].trim();
+		int sal=Integer.parseInt(param[2].trim());
 		
-		String sql = "insert into emp (empno, ename, sal, hiredate) values (" + empno + ", '" + ename + "', " + sal + ", now())";
-		int result = 0;
-		String errMsg = "";
-		
+		String sql="insert into emp (empno,ename,sal,hiredate) values ("+empno+",'"+ename+"',"+sal+",now())";
+		int result=0;
+		String errMsg="";
 		try(
-				Connection conn = BitMy.getConnection();
-				Statement stmt = conn.createStatement();
+			Connection conn=BitMy.getConnection();
+			Statement stmt=conn.createStatement();	
 				){
-			result = stmt.executeUpdate(sql);
+			result=stmt.executeUpdate(sql);
 		} catch (SQLException e) {
 			e.printStackTrace();
-			errMsg = e.getMessage();
+			errMsg=e.getMessage();
 		}
-		
 		resp.setContentType("application/json");
 		PrintWriter pw = resp.getWriter();
 		pw.print("{\"insert\":[{\"result\":");
-		pw.print(result > 0 ? true : false);
-		pw.print(",\"err\":\"" + errMsg);
-		pw.println("\"}]}");
+		pw.print(result>0?true:false);
+		pw.print(",\"err\":\""+errMsg);
+		pw.println("\"}]}"); 
 	}
 }
+
+
+
+
+
+
+
+
+
