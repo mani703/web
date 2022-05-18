827d4f237c068e5820bdfcf1172bdc0d1f9a9678
mani703 <mani703@naver.com>
2022-05-18 14:04:25 +0900

    dto

---
+++ web15/src/main/java/com/bit/model/EmpDto.java
@@ -0,0 +1,41 @@
+package com.bit.model;
+
+import java.sql.Timestamp;
+
+public class EmpDto {
+	private int empno;
+	private String ename;
+	private int sal;
+	private Timestamp hiredate;
+	
+	public int getEmpno() {
+		return empno;
+	}
+	public void setEmpno(int empno) {
+		this.empno = empno;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public int getSal() {
+		return sal;
+	}
+	public void setSal(int sal) {
+		this.sal = sal;
+	}
+	public Timestamp getHiredate() {
+		return hiredate;
+	}
+	public void setHiredate(Timestamp hiredate) {
+		this.hiredate = hiredate;
+	}
+	
+	@Override
+	public String toString() {
+		return "EmpDto [empno=" + empno + ", ename=" + ename + ", sal=" + sal + ", hiredate=" + hiredate + "]";
+	}
+	
+}
