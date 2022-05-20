346ea1911dce2bf3c3a65c0d9dfef6edcb0337ae
mani703 <mani703@naver.com>
2022-05-20 12:48:22 +0900

    mongo crud

--- web17/src/main/java/com/bit/model/EmpDao.java
+++ web17/src/main/java/com/bit/model/EmpDao.java
@@ -1,21 +1,24 @@
 package com.bit.model;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.bson.Document;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 
 public class EmpDao {
+	String addr = "localhost:27017";
+	MongoClient client = null;
 	
-	public List<?> selectAll(){
-		List<?> list = new ArrayList<>();
-		String addr = "localhost:27017";
-		MongoClient client = null;
+	public List<Map<String, Object>> selectAll(){
+		List<Map<String, Object>> list = new ArrayList<>();
 		
 		try {
 			client = new MongoClient(addr);
@@ -23,8 +26,12 @@ public class EmpDao {
 			MongoCollection<Document> rs = db.getCollection("emp");
 			MongoCursor<Document> cur = rs.find().iterator();
 			while(cur.hasNext()) {
+				Map<String, Object> bean = new HashMap<String, Object>();
 				Document doc = cur.next();
-				System.out.println(doc);
+				bean.put("_id", doc.get("_id"));
+				bean.put("empno", doc.get("empno"));
+				bean.put("ename", doc.get("ename"));
+				list.add(bean);
 			}
 			
 		} finally {
@@ -32,5 +39,24 @@ public class EmpDao {
 		}
 		
 		return list;
+	}
+
+	public void insertOne(int empno, String ename) {
+		
+		try {
+			client = new MongoClient(addr);
+			MongoDatabase db = client.getDatabase("testDB");
+			MongoCollection<Document> coll = db.getCollection("emp");
+//			BasicDBObject doc = new BasicDBObject();
+//			doc.append("empno", empno);
+//			doc.append("ename", ename);
+			
+			Document doc = new Document();
+			doc.append("empno", empno);
+			doc.append("ename", ename);
+			coll.insertOne(doc);
+		} finally {
+			if(client != null) client.close();
+		}
 	}
 }
